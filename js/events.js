64661d3cc6db6557f5c6ffd710514f704c1ba207
Arnold Ndiwalana <arnoldndiwalana@gmail.com>
2019-03-21 14:44:27 +0300

    event handlers and listeners

--- js/events.js
+++ js/events.js
@@ -1,19 +1,29 @@
 // Function to modify the text content of the paragraph
 
-
+const p = document.querySelector('p');
+const button = document.querySelector("button");
 const changeText = () =>{
-  const p = document.querySelector('p');
-  p.textContent = "I changed because of an  event listener";
+
+  p.textContent = "Will I change?";
 }
 
-/*const alertText = () =>{
+const alertText = () =>{
   alert('Will I alert ?');
-}*/
+}
 
 //Add event handler as property of the button element
 // Events can be overwritten
 // Listen for click event
-const button = document.querySelector("button");
-button.addEventListener('click',changeText);
+// Multiple event listeners an be addesd to the same event and element
+//button.addEventListener('click',changeText);
+button.addEventListener('click',alertText);
+//  An anonymous fuction on an event listeners
+button.addEventListener('click',() =>{
+  p.textContent = "Will I change ?";
+});
+
+// Remove alertText function from Event listener
+button.removeEventListener('click',alertText);
+
 //button.onclick = changeText;
 //button.onclick = alertText;
